d12a7a0647cb7210ff2e6b96f155e1b5a010fbe3
Walter Hulsebos <walterhulsebos@outlook.com>
2023-02-17 19:21:48 +0100

    PlayerAnimations Animator

--- Y2B3-4_UnityProject/Assets/AmplifyAnimationPack/Scripts/Character/AnimStatesBehavior/AttackStateBehavior.cs
+++ Y2B3-4_UnityProject/Assets/AmplifyAnimationPack/Scripts/Character/AnimStatesBehavior/AttackStateBehavior.cs
@@ -11,7 +11,7 @@ namespace AmplifyAnimationPack
 		private CharacterAnimatorBehavior animBeh;
 
 		// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-		override public void OnStateEnter( Animator animator , AnimatorStateInfo stateInfo , int layerIndex )
+		public override void OnStateEnter( Animator animator , AnimatorStateInfo stateInfo , int layerIndex )
 		{
 			if( charRef == null )
 			{
